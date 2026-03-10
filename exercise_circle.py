defb72f0a812a8f787f3771ad5b7f9e7143f7d43
felipelagos-marmol <flagosmarmol1@mail.austral.edu.ar>
2026-03-10 16:32:08 -0300

    Update exercise_circle.py

--- exercise_circle.py
+++ exercise_circle.py
@@ -1,6 +1,3 @@
-from math import pi
-
-
 def circle():
     """
     Ejercicio 6 - Geometría de Círculo
@@ -10,3 +7,7 @@ def circle():
     2. La circunferencia (2 × π × radio)
     """
     radio = 5
+    import math
+
+    print(math.pi * 5**2)
+    print(2 * math.pi * 5)
